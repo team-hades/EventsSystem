202952d0fc8ea70b0acff02458eb612f3b61ac68
g-yonchev <georgi.d.yonchev@gmail.com>
2015-11-04 21:45:07 +0200

    NinjectConfig.cs

---
+++ EventsSystem/EventsSystem.Server/EventsSystem.Api/App_Start/NinjectConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace EventsSystem.Api
+{
+	using Ninject;
+	using System.Reflection;
+
+	public static class NinjectConfig
+	{
+		public static IKernel CreateKernel()
+		{
+			var kernel = new StandardKernel();
+			kernel.Load(Assembly.GetExecutingAssembly());
+
+			RegisterServices(kernel);
+
+			return kernel;
+		}
+
+		private static void RegisterServices(IKernel kernel)
+		{
+			// kernel.Bind<ISomething>().To<Something>();
+		}
+	}
+}
