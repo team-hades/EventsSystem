37afba769494ebfacc3688e92bdadf4077b5aa81
g-yonchev <georgi.d.yonchev@gmail.com>
2015-11-09 17:09:47 +0200

    Added TownsController.cs

---
+++ EventsSystem/EventsSystem.Server/EventsSystem.Api/Controllers/TownsController.cs
@@ -0,0 +1,26 @@
+﻿namespace EventsSystem.Api.Controllers
+{
+	using System.Linq;
+	using System.Web.Http;
+	using EventsSystem.Data.Data.Repositories;
+
+	public class TownsController : BaseController
+	{
+		public TownsController(IEventsSystemData data)
+			: base(data)
+		{
+		}
+
+		public IHttpActionResult Get()
+		{
+			var towns = this.data.Towns.All();
+			return this.Ok(towns);
+		}
+
+		public IHttpActionResult Get(int id)
+		{
+			var events = this.data.Towns.All().Where(c => c.Id == id).FirstOrDefault().Events.Select(e => e.Name);
+			return this.Ok(events);
+		}
+	}
+}
