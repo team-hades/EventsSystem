49cfb599fd275a863836a47986f928e47c634316
georgimanov <georgimanov@gmail.com>
2015-11-11 13:02:03 +0200

    work on dropbox connection

--- EventsSystem/EventssSystem.Dropbox.ConsoleClient/Program.cs
+++ EventsSystem/EventssSystem.Dropbox.ConsoleClient/Program.cs
@@ -1,11 +1,17 @@
 ﻿namespace EventssSystem.Dropbox.ConsoleClient
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
     using global::Dropbox.Api;
+    using global::Dropbox.Api.Files;
 
     class Program
     {
+        private const string AccessToken = "contact me";
+
         static void Main(string[] args)
         {
             var task = Task.Run((Func<Task>)Program.Run);
@@ -14,10 +20,49 @@
 
         static async Task Run()
         {
-            using (var dbx = new DropboxClient("YOUR ACCESS TOKEN"))
+            using (var dbx = new DropboxClient(AccessToken))
             {
                 var full = await dbx.Users.GetCurrentAccountAsync();
                 Console.WriteLine("{0} - {1}", full.Name.DisplayName, full.Email);
+            }
+        }
+
+        // To list all the contents in the user's root directory:
+        async Task ListRootFolder(DropboxClient dbx)
+        {
+            var list = await dbx.Files.ListFolderAsync(string.Empty);
+
+            // show folders then files
+            foreach (var item in list.Entries.Where(i => i.IsFolder))
+            {
+                Console.WriteLine("D  {0}/", item.Name);
+            }
+
+            foreach (var item in list.Entries.Where(i => i.IsFile))
+            {
+                Console.WriteLine("F{0,8} {1}", item.AsFile.Size, item.Name);
+            }
+        }
+
+        // To download a file:
+        async Task Download(DropboxClient dbx, string folder, string file)
+        {
+            using (var response = await dbx.Files.DownloadAsync(folder + "/" + file))
+            {
+                Console.WriteLine(await response.GetContentAsStringAsync());
+            }
+        }
+
+        // To upload a file:
+        async Task Upload(DropboxClient dbx, string folder, string file, string content)
+        {
+            using (var mem = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            {
+                var updated = await dbx.Files.UploadAsync(
+                    folder + "/" + file,
+                    WriteMode.Overwrite.Instance,
+                    body: mem);
+                Console.WriteLine("Saved {0}/{1} rev {2}", folder, file, updated.Rev);
             }
         }
     }
