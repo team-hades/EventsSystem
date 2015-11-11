b03c01487b3cda813ae3052ed809993434651511
georgimanov <georgimanov@gmail.com>
2015-11-11 12:41:45 +0200

    [Work on] Connect with Dropbox api

---
+++ EventsSystem/EventssSystem.Dropbox.ConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EventssSystem.Dropbox.ConsoleClient
+{
+    using System;
+    using System.Threading.Tasks;
+    using global::Dropbox.Api;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run((Func<Task>)Program.Run);
+            task.Wait();
+        }
+
+        static async Task Run()
+        {
+            using (var dbx = new DropboxClient("YOUR ACCESS TOKEN"))
+            {
+                var full = await dbx.Users.GetCurrentAccountAsync();
+                Console.WriteLine("{0} - {1}", full.Name.DisplayName, full.Email);
+            }
+        }
+    }
+}
