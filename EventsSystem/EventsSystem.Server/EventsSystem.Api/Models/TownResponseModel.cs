1f64d454eec2ae6d0b5470dc23257e0b1248b156
georgimanov <georgimanov@gmail.com>
2015-11-13 00:34:28 +0200

    edit TownResponseModel

--- EventsSystem/EventsSystem.Server/EventsSystem.Api/Models/TownResponseModel.cs
+++ EventsSystem/EventsSystem.Server/EventsSystem.Api/Models/TownResponseModel.cs
@@ -1,10 +1,27 @@
 ﻿namespace EventsSystem.Api.Models
 {
-    using Infrastructure.Mapping;
-    using Data.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
 
-    public class TownResponseModel : IMapFrom<Town>
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class TownResponseModel : IMapFrom<Town>, IHaveCustomMappings
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public List<EventResponseModel> Events
+        {
+            get; set;
+        }
+
+        public void CreateMappings(IConfiguration config)
+        {
+            config.CreateMap<Town, TownResponseModel>()
+                .ForMember(e => e.Events, opts => opts.MapFrom(c => c.Events.ToList()));
+        }
     }
 }
