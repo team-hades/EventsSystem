89b1b0893392bf6aa6edd7e2b40fc7bfdc93eaa0
Chavdar Angelov <c.l.angelov@gmail.com>
2015-11-17 16:37:56 +0200

    EventResponseModelCreated

---
+++ EventsSystem/EventsSystem.Server/EventsSystem.Api/Models/Comments/CommmentsResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace EventsSystem.Api.Models.Comments
+{
+    using System;
+    using AutoMapper;
+    using EventsSystem.Api.Infrastructure.Mapping;
+    using EventsSystem.Data.Models;
+
+    public class CommmentsResponseModel : IMapFrom<Comment>, IHaveCustomMappings
+    {
+        public string Content { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int EventId { get; set; }
+
+        public string UserName { get; set; }
+
+        public void CreateMappings(IConfiguration config)
+        {
+            config.CreateMap<Comment, CommmentsResponseModel>()
+                .ForMember(c => c.UserName, opts => opts.MapFrom(c => c.Author.FirstName));
+        }
+    }
+}
