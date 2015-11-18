51c1aba0502309e763829582927944cf38ade506
georgimanov <georgimanov@gmail.com>
2015-11-18 21:31:20 +0200

    Added TownsController-Tests

---
+++ EventsSystem/EventsSystem.Tests/EventsSystem.Tests.Controllers/TownsControllerShould.cs
@@ -0,0 +1,23 @@
+﻿namespace EventsSystem.Tests.Controllers
+{
+    using MyTested.WebApi;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Api.Controllers;
+    using System;
+    using System.Net.Http;
+
+    [TestClass]
+    public class TownsController
+    {
+        [TestMethod]
+        public void ReturnValidModelState()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/towns")
+                .WithHttpMethod(HttpMethod.Get)
+                .ToValidModelState();
+        }
+    }
+}
