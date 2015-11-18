bf4b0764144380bd896a10a511244f05a5e940bd
georgimanov <georgimanov@gmail.com>
2015-11-18 20:55:13 +0200

    test method added

---
+++ EventsSystem/EventsSystem.Tests/EventsSystem.Tests.Controllers/TagsControllerShould.cs
@@ -0,0 +1,26 @@
+﻿namespace EventsSystem.Tests.Controllers
+{
+    using MyTested.WebApi;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Api.Controllers;
+    using System;
+    using System.Net.Http;
+
+    [TestClass]
+    public class TagsControllerShould
+    {
+        // http://localhost:58368/api/comments/35
+        // public IHttpActionResult PostComment(int eventId, [FromBody]
+
+        [TestMethod]
+        public void ReturnMethodNotAllowed()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/comments/35")
+                .WithHttpMethod(HttpMethod.Get)
+                .ToNotAllowedMethod();
+        }
+    }
+}
