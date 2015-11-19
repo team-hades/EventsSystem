5795b3a7b5ba678720707970e05d85b3f4efd921
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-11-20 00:48:43 +0200

    Towns controller test added

---
+++ EventsSystem/EventsSystem.Api.Tests/ControllersTests/TownControllerTests.cs
@@ -0,0 +1,45 @@
+﻿namespace EventsSystem.Api.Tests.ControllersTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MyTested.WebApi;
+    using Api.Controllers;
+    using System.Net.Http;
+    using Models.Towns;
+
+    [TestClass]
+    public class TownControllerTests
+    {
+        [TestMethod]
+        public void TestGetTownShouldMapCorrectly()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/towns")
+                .To<TownsController>(c => c.Get());
+        }
+
+        [TestMethod]
+        public void TestGetTownIdShouldMapCorrectly()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/towns/1")
+                .To<TownsController>(c => c.Get(1));
+        }
+
+        [TestMethod]
+        public void TestTownsPostShouldMapCorrectly()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/towns")
+                .WithHttpMethod(HttpMethod.Post)
+                .WithJsonContent(@"{ ""Name"": ""City"" }")
+                .To<TownsController>(c => c.Post(new TownSaveModel
+                {
+                    Name = "City"                     
+                }));
+        }
+    }
+}
